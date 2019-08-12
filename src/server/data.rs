fbad42365569a74092c94a2775e0dce2aa1d8d3c
Pierre Colin <48397990+Pierre-Colin@users.noreply.github.com>
2019-08-12 14:42:21 +0200

    Restructure server module as a directory

--- src/server/data.rs
+++ src/server/data.rs
@@ -1,16 +1,6 @@
-use std::{
-    collections::HashSet,
-    fs,
-    io::prelude::*,
-    net::{TcpListener, TcpStream},
-    sync::{Arc, Mutex},
-    thread,
-};
+use std::collections::HashSet;
 
-use super::election_info;
-use super::util;
-
-fn squeeze_json(string: String) -> String {
+pub fn squeeze_json(string: String) -> String {
     let mut result = String::with_capacity(string.len());
     let mut indent = false;
     let mut last_comma = false;
@@ -42,7 +32,7 @@ enum ParseState {
     Done,
 }
 
-fn parse_ballot(s: String, alternatives: &HashSet<String>)
+pub fn parse_ballot(s: String, alternatives: &HashSet<String>)
     -> Option<rcvs::Ballot<String>>
 {
     let mut ballot = rcvs::Ballot::new();
@@ -113,77 +103,4 @@ fn parse_ballot(s: String, alternatives: &HashSet<String>)
         };
     }
     Some(ballot)
-}
-
-fn handle_connection(mut stream: TcpStream,
-                     election: &Mutex<rcvs::Election<String>>,
-                     alternatives: &HashSet<String>,
-                     data: &str,
-                     peers: &Mutex<HashSet<std::net::IpAddr>>)
-{
-    let ip = stream.peer_addr().unwrap().ip();
-    let already_voted = peers.lock().unwrap().contains(&ip);
-    if already_voted {
-        println!("Connection from {} rejected", ip);
-        stream.write(b"VOTED").unwrap();
-        stream.flush().unwrap();
-    } else {
-        println!("Connection from {} granted", ip);
-        stream.write(data.as_bytes()).unwrap();
-        stream.flush().unwrap();
-        let buffer = util::read_packet(&mut stream, 2048)
-            .expect("Failed to receive ballot data");
-        let ballot = parse_ballot(buffer.to_string(), &alternatives).unwrap();
-
-        let mut election = election.lock().unwrap();
-        if peers.lock().unwrap().insert(ip) {
-            election.cast(ballot);
-        } else {
-            println!("Double-vote detected for {}", ip);
-            stream.write(b"VOTED").unwrap();
-            stream.flush().unwrap();
-        }
-
-        println!("{}", election);
-    }
-}
-
-pub fn run(matches: &clap::ArgMatches) {
-    let filename = matches.value_of("ELECTION").unwrap();
-    let data = Arc::new(squeeze_json(fs::read_to_string(filename)
-        .expect("Failed to open election.json")));
-    let structure: election_info::Election = serde_json::from_str(&data)
-        .expect("Failed to parse JSON data");
-    println!("{}", structure);
-
-    let ip_address = format!("0.0.0.0:{}", matches.value_of("port").unwrap());
-    println!("Opening TCP listener on {}", ip_address);
-    let listener = TcpListener::bind(ip_address).unwrap();
-    let peers = Arc::new(Mutex::new(HashSet::<std::net::IpAddr>::new()));
-
-    let alternatives = Arc::new({
-        let mut temp = HashSet::new();
-        for x in structure.iter() {
-            temp.insert(x.name());
-        }
-        temp
-    });
-    let election = Arc::new(Mutex::new(rcvs::Election::<String>::new()));
-
-    for stream in listener.incoming() {
-        let alternatives = Arc::clone(&alternatives);
-        let data = Arc::clone(&data);
-        let election = Arc::clone(&election);
-        let peers = Arc::clone(&peers);
-
-        thread::spawn(move || {
-            handle_connection(stream.unwrap(),
-                              &election,
-                              &alternatives,
-                              &data,
-                              &peers);
-        });
-    }
-
-    println!("Closing TCP listener");
 }
---
+++ src/server/mod.rs
@@ -0,0 +1,87 @@
+mod data;
+
+use std::{
+    collections::HashSet,
+    fs,
+    io::prelude::*,
+    net::{TcpListener, TcpStream},
+    sync::{Arc, Mutex},
+    thread,
+};
+
+use super::election_info;
+use super::util;
+
+fn handle_connection(mut stream: TcpStream,
+                     election: &Mutex<rcvs::Election<String>>,
+                     alternatives: &HashSet<String>,
+                     data: &str,
+                     peers: &Mutex<HashSet<std::net::IpAddr>>)
+{
+    let ip = stream.peer_addr().unwrap().ip();
+    let already_voted = peers.lock().unwrap().contains(&ip);
+    if already_voted {
+        println!("Connection from {} rejected", ip);
+        stream.write(b"VOTED").unwrap();
+        stream.flush().unwrap();
+    } else {
+        println!("Connection from {} granted", ip);
+        stream.write(data.as_bytes()).unwrap();
+        stream.flush().unwrap();
+        let buffer = util::read_packet(&mut stream, 2048)
+            .expect("Failed to receive ballot data");
+        let ballot =
+            data::parse_ballot(buffer.to_string(), &alternatives).unwrap();
+
+        let mut election = election.lock().unwrap();
+        if peers.lock().unwrap().insert(ip) {
+            election.cast(ballot);
+        } else {
+            println!("Double-vote detected for {}", ip);
+            stream.write(b"VOTED").unwrap();
+            stream.flush().unwrap();
+        }
+
+        println!("{}", election);
+    }
+}
+
+pub fn run(matches: &clap::ArgMatches) {
+    let filename = matches.value_of("ELECTION").unwrap();
+    let data = Arc::new(data::squeeze_json(fs::read_to_string(filename)
+        .expect("Failed to open election.json")));
+    let structure: election_info::Election = serde_json::from_str(&data)
+        .expect("Failed to parse JSON data");
+    println!("{}", structure);
+
+    let ip_address = format!("0.0.0.0:{}", matches.value_of("port").unwrap());
+    println!("Opening TCP listener on {}", ip_address);
+    let listener = TcpListener::bind(ip_address).unwrap();
+    let peers = Arc::new(Mutex::new(HashSet::<std::net::IpAddr>::new()));
+
+    let alternatives = Arc::new({
+        let mut temp = HashSet::new();
+        for x in structure.iter() {
+            temp.insert(x.name());
+        }
+        temp
+    });
+    let election = Arc::new(Mutex::new(rcvs::Election::<String>::new()));
+
+    for stream in listener.incoming() {
+        let alternatives = Arc::clone(&alternatives);
+        let data = Arc::clone(&data);
+        let election = Arc::clone(&election);
+        let peers = Arc::clone(&peers);
+
+        thread::spawn(move || {
+            handle_connection(stream.unwrap(),
+                              &election,
+                              &alternatives,
+                              &data,
+                              &peers);
+        });
+    }
+
+    println!("Closing TCP listener");
+}
